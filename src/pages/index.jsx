d48133dfec23163b74e71a34f441e8745b28ec3e
Pelic <pelicwei@gmail.com>
2021-11-15 16:47:02 +0800

    feat: 測試button大小

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,15 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <div>
+      <h1>Hi people</h1>
+    </div>
     <p className={"text-2xl"}>Welcome to your new Gatsby site.</p>
+    <button
+      className={`bg-green-500 text-white text-sm h-8 rounded-lg px-3 focus:outline transition ease-in-out duration-150`}
+    >
+      Button
+    </button>
     <p>Now go build something great.</p>
   </Layout>
 )
